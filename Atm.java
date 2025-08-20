06e2f74cca87729a95e19982555d966f32449a51
anirudhchakraborty <preetiujjal@gmail.com>
2025-08-21 01:34:12 +0530

    Withdraw method created and added

--- Atm.java
+++ Atm.java
@@ -9,7 +9,6 @@ public class Atm {
     static Atm callAtm = new Atm();
     static int lastDigit = 01;
 
-
     // while boolean to have the loop running till it stops
 
     static boolean whileRunning = true;
@@ -21,7 +20,7 @@ public class Atm {
         callAtm.Loginmenu();
 
         while (whileRunning) {
-            
+
             callAtm.MainMenu();
             int optionChosen = sc.nextInt();
             sc.nextLine();
@@ -30,7 +29,10 @@ public class Atm {
                 case 1:
                     callAtm.Create();
                     break;
-                case 5: 
+                case 2:
+                    callAtm.Withdraw();
+                    break;
+                case 5:
                     System.out.println("Quitting the Program:");
                     whileRunning = false;
                 default:
@@ -40,6 +42,7 @@ public class Atm {
 
     }
 
+    // The very first Menu for Login
     void Loginmenu() {
         System.out.println();
         System.out.println("====== Welcome to Atm App ======");
@@ -57,6 +60,7 @@ public class Atm {
 
     }
 
+    // The Main Menu for Cards
     void MainMenu() {
         System.out.println("====== Welcome to Atm App ======");
         System.out.println();
@@ -70,10 +74,11 @@ public class Atm {
 
     }
 
+    // Creating New Card
     void Create() {
         System.out.println("==== Creating New Card ====");
 
-        String card = String.valueOf(10000000 + lastDigit);
+        String card = String.valueOf(100 + lastDigit);
         lastDigit++;
 
         System.out.println();
@@ -81,14 +86,12 @@ public class Atm {
         System.out.println();
 
         System.out.print("Set your 4-digit PIN: ");
-        int pin = sc.nextInt();
-        sc.nextLine();
+        String pin = sc.nextLine();
         System.out.println();
 
         System.out.println("Please Enter your Name");
         String name = sc.nextLine();
         System.out.println();
-
 
         System.out.println("Enter initial deposit: ");
         double amount = sc.nextDouble();
@@ -108,23 +111,82 @@ public class Atm {
         System.out.println("Yes or No");
 
         String cont = sc.nextLine();
-        if (cont.charAt(0)== 'n'|| cont.charAt(0) == 'N') {
+        if (cont.charAt(0) == 'n' || cont.charAt(0) == 'N') {
             System.out.println("quitting program");
             whileRunning = false;
         }
 
     }
 
+    // Withdrawl System
+    void Withdraw() {
+
+        System.out.println("Enter card Number");
+        String cardNumber = sc.nextLine();
+
+        System.out.println(" Enter Pin ");
+        String pin = sc.nextLine();
+
+        System.out.println("Amount to Withdraw");
+        Double withdrawl = sc.nextDouble();
+        sc.nextLine();
+        boolean runner = true;
+
+        while (runner) {
+            if (account.containsKey(cardNumber)) {
+
+                if (account.get(cardNumber).pin.equals(pin)) {
+                    System.out.println("Correct Pin!!");
+
+                    if (account.get(cardNumber).balance >= withdrawl) {
+                        System.out.println();
+                        
+                        System.out.println(withdrawl + " Disbursed Succesfully");
+                        account.get(cardNumber).balance -= withdrawl;
+
+                        System.out.println();
+                        System.out.println("Remaining Balnce is: " + (account.get(cardNumber).balance));
+                        
+                        System.out.println();
+                        System.out.println(" Do You wish to Go to Main Menu ");
+                        System.out.println("Yes or No");
+
+                        String cont = sc.nextLine();
+                        if (cont.charAt(0) == 'n' || cont.charAt(0) == 'N') {
+                            System.out.println("quitting program");
+                            whileRunning = false;
+                        } else {
+                            runner = false;
+                            return;
+                        }
+                    } else {
+                        System.out.println("Not Sufficient Balance");
+                        System.out.println("Enter correct Amount");
+                        withdrawl = sc.nextDouble();
+                        sc.nextLine();
+                    }
+                } else {
+                    System.out.println("**** Invalid Pin ****");
+                    System.out.println("ReEnter Pin!!");
+                    pin = sc.nextLine();
+                }
+            } else {
+                System.out.println("Invalid Credentials");
+                System.out.println("Retype Card number");
+                cardNumber = sc.nextLine();
+            }
+        }
+    }
 }
 
 class Accounts {
 
     String name;
-    int pin;
-    double balance;
+    String pin;
+    Double balance;
     String cardNumber;
 
-    Accounts(String name, int pin, double balance, String cardNumber) {
+    Accounts(String name, String pin, Double balance, String cardNumber) {
 
         this.name = name;
         this.cardNumber = cardNumber;
