3170693b4958da7865d9a984639270b09ee4451f
anirudhchakraborty <preetiujjal@gmail.com>
2025-08-20 09:21:22 +0530

    File Created, Acounts Created next section Work Goin on

---
+++ Atm.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Atm {
+//Main class main method
+    public static void main(String[] args) {
+    //main method that runs and calls
+    
+        menu();
+
+    }
+
+    static void menu(){
+        System.out.println();
+        System.out.println("====== Welcome to Atm App ======");
+        System.out.println();
+        System.out.println("======= Do you have card =======");
+    }
+
+}
+
+
+
